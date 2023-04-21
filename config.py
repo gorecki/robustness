ea0fb735956cde3f37523b12fd50b0fc4f9ceaa8
gorecki <gorecki@opf.slu.cz>
2023-04-21 14:59:51 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,231 @@
+'''
+Configuration of the project (used by robustness.py). 
+Also provides classes implementing priority vectors, perturbations and methods.
+
+Limitations:
+Supports only two classes of the distribution of perturbations (Normal and Skewed Normal).
+
+Careful:
+All methods (EVM, GMM, COH) are implemented assuming the Additive model! 
+That is, reciprocal PCMs are with 0 on the diagonal, and PVs, if normalized, are adjusted such that sum(PV) = 0.
+
+Copyright 2023 Jan Gorecki
+'''
+
+from scipy.stats import norm, skewnorm
+import numpy as np
+from abc import ABC, abstractmethod
+
+
+
+class PriorityVector(np.ndarray):
+    '''Container for priorities'''
+    def __new__(cls, input_array):
+        obj = np.asarray(input_array).view(cls) # assure that an instance of PriorityVector is returned (rather than of ndarray)
+        return obj
+
+
+    def rank(self):
+        # returns rank ordering of the PV
+        return np.argsort(np.argsort(self))
+
+
+    def are_weights_unique(self):
+        # given vector pv, checks if all its elemenets are different, and returns True if so, False otherwise
+        unique = len(set(self)) == len(self)
+        return unique
+
+
+    def get_consistent_pcm(self):
+        # Returns a PCM consistent with the priority vector (self)
+        n = len(self)
+        pcm = np.zeros((n, n))
+        for i in range(n):
+            for j in range(i+1, n): # iterate just above the diagonal
+                pcm[i, j] = self[i] - self[j]
+                pcm[j, i] = -pcm[i, j]          # reciprocity
+        return pcm
+
+
+
+class Perturbation:
+    '''Model for random perturbations'''
+    def __init__(self, name, sigma, loc = 0, a = None, force_zero_mean = False):
+        '''
+        name                - name of the distribution modeling the perturbations (in ['Normal', 'Skewnorm'])
+        sigma               - spread of the perturbations
+        loc                 - location of the perturbations
+        a                   - skewness of the perturbations
+        force_zero_mean     - by setting to True, zero mean of the perturbations is assured
+        '''
+        self.name = name
+        assert self.name in ['Normal', 'Skewnorm'], "name NOT in ['Normal', 'Skewnorm']!"
+        if self.name == 'Normal':
+            self.sigma = sigma # uncertainty of the decision maker -  std of normal distr for perturbation
+            self.loc = loc # mean of perturbation
+            if force_zero_mean and loc != 0:
+                raise Exception(f'loc != 0 but {force_zero_mean=}!')
+        else: # name == 'Skewnorm':
+            self.a = a # skewness
+            self.sigma = sigma # uncertainty of the decision maker  
+            if force_zero_mean:# assure mean = 0?
+                mean = skewnorm.stats(a, loc = loc, scale = sigma, moments='m')
+                self.loc = -mean # center the distribution, i.e., mean = 0
+                print(f'loc shifted from {loc} to {self.loc}')
+            else: # mean if free
+                self.loc = loc
+
+
+    def fname(self):
+        sigma = f"sigma_{str(self.sigma).replace('.','_')}"
+        if self.name == 'Normal':
+            return sigma
+        else:
+            return f"a_{str(self.a).replace('.','_')}_" + sigma 
+
+
+    def repr(self):
+        return f"{self.n}_N_{self.N}_{self.name}_" + self.fname()
+
+
+    def plot_pdf(self, ax, ls):
+        a = 0 if self.name == 'Normal' else self.a # norm is skewnorm with a = 0
+        x = np.linspace(-5, 5, 50)
+        ax.plot(x, skewnorm.pdf(x, a, loc = self.loc, scale = self.sigma),  'brown', ls = ls, lw=2, alpha=0.6, label='skewnorm pdf')
+
+
+    def rnd(self):
+        if self.name == 'Normal': 
+            return norm.rvs(loc = self.loc, scale = self.sigma, size = 1)
+        else: # 'Skewnorm' 
+            return skewnorm.rvs(self.a, loc = self.loc, scale = self.sigma, size = 1)
+
+
+    def eval_sf(self, value): # 
+        # evaluate the survival function (sf = 1-cdf) if the perturbation
+        if self.name == 'Normal':
+            return norm.sf(value, loc = self.loc, scale = self.sigma) # formula from the paper 
+        else:
+            return skewnorm.sf(value, self.a, loc = self.loc, scale = self.sigma) 
+
+
+
+class Method(ABC):
+    ''' Template for methods that calculate the rank ordering of a PV for a reciprocal PCM in the additive model.
+    In the paper notation, r(M(A)) is calculated by M.pcm2ranks(A), where M is an instance of Method.
+    '''
+    
+    def __init__(self, name, color):
+        self.name = name     # abbrevitation
+        self.color = color   # color in plots
+
+
+    @abstractmethod
+    def pcm2ranks(self, pcm):
+        pass
+
+
+    def __repr__(self):
+        return self.name
+
+
+
+class SaatyEigenvectorMethod(Method):
+    def __init__(self):
+        super().__init__('EVM', 'r')
+
+
+    def pcm2ranks(self, pcm):
+        # pcm is assumed to be in additive model
+        mult_pcm = np.exp(pcm) # strictly positive matrix
+        eig_values, eig_vectors = np.linalg.eig(mult_pcm)
+        i_lambda_max = np.argmax(eig_values)
+        mult_w = PriorityVector(eig_vectors[:, i_lambda_max].real) # eigenvector corrsponding to lambda_max
+                                                   # note that as mult_pcm is > 0, we know that all elements
+                                                   # of the eigenvector corresponding to the largest eigenvalue are real of the same sign
+        if mult_w[0] < 0:
+            mult_w = -mult_w # make all elementes positive (as we know that now all have to be negative!)
+        assert (mult_w > 0).all(), 'mult_w > 0 violated!'
+        return mult_w.rank() # due to the monotonic isomorphism exp(), the we don't need, to waste time by transforming back to the additive model
+
+
+
+class GeometricMeanMethod(Method):
+    def __init__(self):
+        super().__init__('GMM', 'g')
+
+
+    def pcm2ranks(self, pcm):
+        # geometric mean method for the additive model, i.e, v_i = (sum_j=1...n (a_ij)) / n
+        # NOTE: v is inherently normalized (note that sum(pcm) = 0 due to reciprocity, 
+        #       hence sum of elements of v must also equal to 0)
+        return PriorityVector(np.sum(pcm, axis = 1) / pcm.shape[0]).rank()
+
+
+
+class CoherencyMethod(Method):
+    def __init__(self):
+        super().__init__('COH', 'b')
+
+
+    def pcm2ranks(self, pcm):
+        ranks = (pcm > 0).sum(axis = 1) # sum of indicators - NOTE: due to Python standards, we don't add +1 to the ranks and start counting from 0
+        if not PriorityVector(ranks).are_weights_unique():
+            ranks = None # the method does not produce any output if the pcm is not coherent, which is detected
+                     # by the uniqueness of ranks
+        return ranks
+
+
+
+# methods to include in the comparison
+methods = [SaatyEigenvectorMethod(),
+           GeometricMeanMethod(),
+           CoherencyMethod()]
+
+
+class Cfg:
+    ''' Project configuration'''
+    def __init__(self, N, do_random_v_space, zero_mean_exp, load_results, save_fig, plot_type): 
+        '''
+        N                   - number of replications
+        do_random_v_space   - if False, compute the v-robustness. If true, compute the robustness
+        zero_mean_exp       - True, to replicate the zero mean perturbation results, False for non-zero mean perturbation results
+        load_results        - if False, compute new results and save. If True, just load previously saved results.
+        save_fig            - if False, just show the figure without saving, if True, save the figure without showing
+        '''
+
+        # methods involved in the comparison
+        self.methods = methods
+
+        self.N = N
+        self.do_random_v_space = do_random_v_space
+        self.zero_mean_exp = zero_mean_exp
+        self.load_results = load_results
+        self.save_fig = save_fig
+        assert plot_type in ['analytical', 'numerical', None], "plot_type NOT in ['analytical', 'numerical', None]!"
+        self.plot_type = plot_type
+
+        if not self.do_random_v_space:
+            self.ns = [3] # PCM dimesions
+            self.sigmas = [1/2, 1, 2]
+            # non-random 1D v2space
+            if self.zero_mean_exp:
+                self.aas = [-5, 0, 5] # skewness parameters
+            else: # non zero_mean_exp
+                self.locs = [-1, -1/2, 1/2, 1]
+        else:
+            self.ns = range(3, 7) # PCM dimensions
+            self.sigmas = [0.05, 0.1, 0.2, 0.5, 1]
+            if self.zero_mean_exp:
+                self.aas = [0] # just N(0, sigma^2)
+            else: # non zero_mean_exp
+                raise Exception('Not considered in our work.')
+
+
+    def get_perturbations_model(self, sigma, a = None, loc = None):
+        if self.zero_mean_exp:
+            assert a is not None, 'a is None!'
+            return Perturbation('Skewnorm', sigma, a = a, force_zero_mean = True)
+        else:
+            assert loc is not None, 'loc is None!'
+            return Perturbation('Normal', sigma, loc = loc, force_zero_mean = False)
---
+++ robustness.py
@@ -0,0 +1,382 @@
+'''
+Unifying framework for evaluating robustness of pairwise comparison methods against rank reversal
+
+Implementing the framework developed in:
+Jan Gorecki, David Bartl and Jaroslav Ramik (2023). Robustness of methods calculating priority 
+vectors from pairwise comparison matrices against rank reversal: A probabilistic concept
+(below referred to as 'the paper')
+
+Copyright 2023 Jan Gorecki
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+from time import time
+import os
+
+from config import Cfg, PriorityVector
+
+
+
+def add_perturbations(pcm, perturbation):
+    # Add perturbations to a consistent PCM pcm
+    perturbed_pcm = pcm.copy()
+    n = pcm.shape[0]
+    for i in range(n):
+        for j in range(i+1, n): # iterate just above the diagonal
+            X_ij = perturbation.rnd()                   # generate random perturbation
+            perturbed_pcm[i, j] = pcm[i, j] + X_ij      # add it to the consistent matrix 
+            perturbed_pcm[j, i] = -perturbed_pcm[i, j]  # keep reciprocity
+    return perturbed_pcm
+
+
+def analytical_COH_robustness(pvec, perturbation):
+    # Computes the v-robustness of the COH method using the analytical
+    # approach developed in the paper (only for n = 3)
+    assert pvec.are_weights_unique(), f'weights in {pvec=} are not unique!'
+    assert pvec[0] > pvec[1] > pvec[2], 'assumption of pvec[0] > pvec[1] > pvec[2] violated!'
+    pcm = pvec.get_consistent_pcm()
+    r = 1
+    n = pvec.shape[0]
+    for i in range(n):
+        for j in range(i+1, n):
+            assert pcm[i, j] > 0, 'pcm[i, j] <= 0!'
+            r *= perturbation.eval_sf(-pcm[i, j])
+    return r 
+
+
+def create_dir_if_not_exists(dir_name):
+    # Check if the directory exists abd if not, create it
+    if not os.path.exists(dir_name):
+        # Create the directory if it does not exist
+        os.makedirs(dir_name)
+        print(f"Directory {dir_name} created successfully")
+
+
+###### Monte Carlo simulations #############################
+
+
+def replication_coh(PVgenerator, cfg, perturbation):
+    # Perform one replication of the MC approach developed in the paper.
+    # For all methods involved in cfg, computes if there is a rank reversal 
+    # after perturbations to the consistent PCM are added 
+    results = {'n_all_reps': 0}
+    for method in cfg.methods:
+        results['n_equal_ranks_' + method.name] = 0 
+        results['n_' + method.name] = 0
+    do_rep = True
+    # repeat until a coherent noisy PCM if generated (computing the v-robustness conditioned on that the 
+    # generated PCM is coherent)
+    while do_rep:
+        pv = PVgenerator()             # generate the "uknown" PV 
+        if not pv.are_weights_unique(): # uniqueness check
+            continue # some of weights are duplicate, so repeat     
+        pcm = pv.get_consistent_pcm()                            # generate consistent PCM from random vector
+        noisy_pcm = add_perturbations(pcm, perturbation)         # add some noise to that consistent PCM
+
+        ranks = {} # ranks for each method
+        for method in cfg.methods:
+            ranks[method.name] = method.pcm2ranks(noisy_pcm)
+
+        # check uniqueness of outputted ranks (except for COH)
+        all_unique = True
+        for method in cfg.methods:
+            if method.name != 'COH': # we don't test COH as it is either unique or None
+                if not ranks[method.name].are_weights_unique():
+                    all_unique = False
+                    print(f'{method.name} generated non-unique ranks {ranks[method.name]}')
+        if not all_unique:
+            continue # some output was non-unique, so repeat the replication
+
+        results['n_all_reps']  += 1 # we have obtained unique PVs from all methods (note that COH could return None, which is Ok)
+
+        if ranks['COH'] is None:
+            # the noisy_pcm is not coherent, so repeat the replication
+            continue
+
+        # now, we know that noisy_pcm is coherent, so we can do all statistics 
+        true_ranks = pv.rank()
+        for method in cfg.methods:
+            results['n_' + method.name] += 1 # count the method outputs
+            results['n_equal_ranks_' + method.name] += (ranks[method.name] == true_ranks).all() # +1 if all ranks match, +0 otherwise
+
+        do_rep = False # all statistics done, so we don't further repeat the replication
+
+    return results
+
+
+def exp_core(PVgenerator, cfg, perturbation):
+    # Repeat replication_coh() cfg.N times and sum up the results
+    results = {'n_all_reps': 0}
+    for method in cfg.methods:
+        results['n_equal_ranks_' + method.name] = 0 
+        results['n_' + method.name] = 0
+    for i in range(cfg.N):
+        i_results = replication_coh(PVgenerator, cfg, perturbation) # jen na coherent 
+        for method in cfg.methods:
+            results['n_equal_ranks_' + method.name] += i_results['n_equal_ranks_' + method.name]
+            results['n_' + method.name] += i_results['n_' + method.name]
+        results['n_all_reps'] += i_results['n_all_reps']
+    return results
+
+
+
+def exp_prob(cfg, perturbation, n, v1, v2_space, v3):
+    # iterates of the space of pririty vectors (represented here by v1, v2_space, v3)
+    # and computes the robustness via our MC apprach and eventually also via our analytical 
+    # approach for the COH method
+    robustness = {}
+    for rob_type in ['rough']:
+        for method in cfg.methods:
+            robustness[f'cond_{rob_type}_' + method.name] = []
+    robustness.update({'rough_COH': [],
+                       'rough_analyt_COH': [], # we want to order the keys like this
+                       'COH_ratio': []})
+    if cfg.do_random_v_space: 
+        del robustness['rough_analyt_COH'] # we don't have the ordering v3 > v2 > v1 so we don't use the analytic formula
+    for v2 in v2_space:
+        #print(v2)
+        if cfg.do_random_v_space:
+            exp_prob_PVgenerator = lambda : PriorityVector(np.random.uniform(low = v1, high = v3, size = n))
+        else: #non-random v_space
+            exp_prob_PVgenerator = lambda : PriorityVector(np.concatenate((v3, v2, v1), axis=None)) # v3 > v2 > v1 to satisfy the assumptions for analytical_COH_robustness()
+        
+        results = exp_core(exp_prob_PVgenerator, cfg, perturbation)
+        
+        for method in cfg.methods:
+            robustness['cond_rough_' + method.name].append(results['n_equal_ranks_' + method.name]/results['n_' + method.name])   # rough robustness conditioned on isCOH
+        robustness['rough_COH'].append(results['n_equal_ranks_COH']/results['n_all_reps'])                # numerical equivalent to the analalytical case
+        if 'rough_analyt_COH' in robustness:
+            robustness['rough_analyt_COH'].append(analytical_COH_robustness(exp_prob_PVgenerator(), perturbation))
+        robustness['COH_ratio'].append(cfg.N / results['n_all_reps']) # #coherent/#all
+        #COHdebug(v)
+
+    print(f"n_all_reps = {results['n_all_reps']}") # just for the last v2_space point
+    return robustness
+
+
+########### plots ##############################
+
+
+def plot_3D_one_type_R(robustness, cfg, v1, v2_space, v3):
+    # creates figures showing the v-robustness computed via our MC approach for n = 3
+    n = 3
+    robustness = robustness[n] # robustness is computed just for n = 3, so take just these results
+
+    first_sigma = list(robustness.keys())[0] # used to access all a NOTE: set of a is the same for all sigma!!
+    n_col = len(robustness[first_sigma]) # each param a in a separate column
+    scale = 3
+    fig, axs = plt.subplots(3, n_col, figsize=(n_col * scale, 3 * scale))
+    sigma_line_style = ('-', '-.', '--')
+    legend = []
+    legend_rat = []
+    for i_a, a in enumerate(robustness[first_sigma]):
+        for i_sigma, sigma in enumerate(robustness):
+            for method in cfg.methods:
+                ax = axs[0, i_a]
+                ax.plot(v2_space, robustness[sigma][a]['cond_rough_' + method.name], ls = sigma_line_style[i_sigma], 
+                        color = method.color, alpha = 1, lw = 2 if method.name == 'EVM' else 1)
+                if i_a == 0:
+                    legend.append(f'{method.name} ' r'$\sigma$=' + f'{sigma}')
+            # coherence ratio
+            ax = axs[1, i_a]
+            ax.plot(v2_space, robustness[sigma][a]['COH_ratio'], ls = sigma_line_style[i_sigma], color = 'm', alpha = 0.6)
+            if i_a == 0:
+                legend_rat.append(r'$\sigma$=' + f'{sigma}')
+            perturbation = cfg.get_perturbations_model(sigma, a = a, loc = a) # NOTE (on loc = a): for zero_mean_exp = False, "a" is in fact "loc"
+            perturbation.plot_pdf(axs[2, i_a], sigma_line_style[i_sigma]) 
+
+        for i in [0, 1]: # iterate one column
+            ax = axs[i, i_a]
+            ax.set_xlim((v1, v3))
+            ax.set_ylim((0, 1.01))
+            ax.set_xlabel('$v_2$')
+            if i == 0:
+                title = f'{a=}' if cfg.zero_mean_exp else f'$\mu$={a}' # the same small hack
+                ax.set_title(title)
+    axs[0, 0].legend(legend, fontsize = 6)
+    axs[1, 0].legend(legend_rat)
+    axs[1, 1].set_title((' ' * (0 if cfg.zero_mean_exp else 55)) + 'Coherency ratio\n')
+    axs[2, 0].legend(legend_rat)
+    axs[2, 1].set_title((' ' * (0 if cfg.zero_mean_exp else 55)) + 'Perturbations PDF')
+    fig.suptitle('(1, $v_2$, 0)-robustness')
+    fig.tight_layout()
+
+
+def plot_nD_one_type_R(robustness, cfg):
+    # creates figures showing the robustness computed via our MC approach 
+    size = 3
+    fig, axes = plt.subplots(1, len(robustness.keys()), figsize=(len(robustness.keys()) * size, size + 2)) 
+    x = np.arange(len(cfg.sigmas))  # the label locations
+    width = 0.8/len(cfg.methods)
+    for i_n, n in enumerate(robustness): # n in ns
+        ax = axes[i_n]
+        rects = {}
+        for i_method, method in enumerate(cfg.methods):
+            r = [] # mean of all robustnesses computed on all random Vs
+            for r_sigma in robustness[n].values():
+                r.append(np.mean(r_sigma[0]['cond_rough_' + method.name]))
+            ax.bar(x + (i_method - 1) * width, r, width, color = method.color, label = method.name)
+
+        if i_n == 0:
+            ax.set_ylabel('Robustness')
+        ax.set_title(f'{n=}')
+        ax.set_xticks(x)
+        ax.set_xticklabels(cfg.sigmas)
+        ax.set_xlabel(r'$\sigma$')
+        ax.set_ylim((0, 1))
+        ax.legend()
+    fig.tight_layout()
+
+
+def plot_3D_analyt_COH(cfg):
+    # creates the figure including the v-robustness accessed analytically for the COH method
+    n = 3
+    size = 4 # plot size
+    fig, ax = plt.subplots(1, 1, figsize=(size,size))
+    N_line_style = {100: ('magenta', ':'),
+                    1000: ('orange', '-.'),
+                    10000: ('green', '--'),
+                    'analytic': ('black', '-')}
+    for N, (color, line_style) in N_line_style.items():
+        if type(N) == int:
+            assert cfg.zero_mean_exp == True, "this plot serves just for cfg.zero_mean_exp = True exps"
+            zero_mean_exp_f_addon = f'_ZME_{cfg.zero_mean_exp}'
+            do_random_v_space_f_addon = f'_randv2_{cfg.do_random_v_space}'
+            res_fname = f'results/res_N_{N}{zero_mean_exp_f_addon}{do_random_v_space_f_addon}.bin'
+            with open(res_fname, 'rb') as handle:
+                robustness, v1, v2_space, v3 = pickle.load(handle)    
+            robustness = robustness[n] # robustness is computed just for n = 3, so take just these results
+            method_name = 'rough_COH'
+        else:
+            # we already loaded file with the analytical results
+            method_name = 'rough_analyt_COH'
+
+        sigma = 0.5
+        a = 0 # Normal distribution
+        if method_name == 'rough_analyt_COH':
+            ax.plot(v2_space, robustness[sigma][a][method_name], ls = '-', color = 'k', lw = 1.5, label = 'analytic')
+        else:
+            ax.scatter(v2_space, robustness[sigma][a][method_name], ls = '-', color = color, marker = 'o', alpha = 0.6, label = f'{N=}')
+    ax.set_xlim((v1, v3))
+    ax.set_ylim((0, 1.01))
+    ax.set_title(f'Analytical vs numerical\nrobustness of COH')
+    ax.set_xlabel('$v_2$')
+    ax.set_ylabel('Robustness of $(1, v_2, 0)$')
+    handles, labels = plt.gca().get_legend_handles_labels()
+    order = [0,1,2,3]
+    ax.legend([handles[idx] for idx in order],[labels[idx] for idx in order])
+    #ax.legend(legend) 
+    fig.tight_layout()
+
+
+###### Simulation wrappers ##################################
+
+
+def exp_prob_inner_wrapper(cfg, n, v1, v2_space, v3):
+    # For the given dimension of PCMs (n), iterates over all parameters
+    # of the distributions of perturbations and for each one computes 
+    # an estimate of the robustness using the MC approach developed in the paper
+    robustness = {} # all results
+    for sigma in cfg.sigmas:
+        robustness[sigma] = {}
+        second_params = cfg.aas if cfg.zero_mean_exp else cfg.locs # iterate either over aas or over locs
+        for second_param in second_params:
+            perturbation = cfg.get_perturbations_model(sigma, a = second_param, loc = second_param)
+            print(f'Computing {sigma=} {second_param=} ...')
+            robustness[sigma][second_param] = exp_prob(cfg, perturbation, n, v1, v2_space, v3)
+    return robustness
+
+
+def exp_prob_wrapper(cfg):
+    # Main procedure that computes the results for a given cfg
+    # Iterate over all dimensions (sizes) of PCMs (cfg.ns) and
+    # computes the robustness for all parametrizations of the perturbations
+    # Once done, a visualization is shown or saved.
+    zero_mean_exp_f_addon = f'_ZME_{cfg.zero_mean_exp}'
+    do_random_v_space_f_addon = f'_randv2_{cfg.do_random_v_space}'
+    res_dir = "results"
+    create_dir_if_not_exists(res_dir)
+    res_fname = f'{res_dir}/res_N_{cfg.N}{zero_mean_exp_f_addon}{do_random_v_space_f_addon}.bin'
+    if cfg.load_results:
+        with open(res_fname, 'rb') as handle:
+            robustness, v1, v2_space, v3 = pickle.load(handle)    
+    else: # compute results
+        t_start = time()
+        v1 = 0
+        v3 = 1
+        robustness = {} # robustness over all ns
+        np.random.seed(0) # replicability 
+        for n in cfg.ns:
+            print(f'{n=}')
+            if not cfg.do_random_v_space: 
+                v2_space = np.linspace(v1 + 0.01, v3 - 0.01, 21)
+            else: # cfg.do_random_v_space = True
+                v2_space = [None] # None here represents a random v point 
+            robustness[n] = exp_prob_inner_wrapper(cfg, n, v1, v2_space, v3) 
+        print(f'Computed in {int(time()-t_start)} s')
+        # save results
+        with open(res_fname, 'wb') as handle:
+            pickle.dump((robustness, v1, v2_space, v3), handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    # plots
+    if cfg.plot_type: 
+        if cfg.plot_type == 'numerical':
+            if cfg.do_random_v_space:
+                plot_nD_one_type_R(robustness, cfg)
+                fig_name = f'probnD_COH_cond'
+            else: # non random v2space
+                plot_3D_one_type_R(robustness, cfg, v1, v2_space, v3)
+                fig_name = 'prob3D_COH_cond'
+        else: # cfg.plot_type == 'analytical'
+            plot_3D_analyt_COH(cfg)
+            fig_name = 'prob3D_COH_analyt'
+        # save or show
+        if cfg.save_fig:
+            # Define the directory name
+            dir_name = "figures"
+            create_dir_if_not_exists(dir_name)
+            fname = f"{dir_name}/{fig_name}{zero_mean_exp_f_addon}"
+            plt.savefig(fname, dpi = 400)
+            print(f'{fname} saved.')
+        else:
+            plt.show(block = True)
+
+
+def replicate():
+    # Switch to True in order to replicate the desired result(s)
+    to_replicate = {'Figure1': True, 
+                    'Figure2': True,
+                    'Figure3': True,
+                    'Figure4': True}
+
+    if to_replicate['Figure1']:
+        for N in [100, 1000, 10000]:
+            cfg = Cfg(N = N, do_random_v_space = False, zero_mean_exp = True, 
+                      load_results = False, save_fig = True, plot_type = 'analytical' if N == 10000 else None) 
+            # NOTE: (explaining plot_type) we first compute the results for N in [100, 1000] without showing the plot, and 
+            #       then for N = 10000 we compute the results as well as we show the plot 
+            exp_prob_wrapper(cfg) # Takes roughly 10 minutes to compute (on Intel(R) Core(TM) i7-7700 CPU @3.60GHz and 32GB RAM)
+                                  
+
+    if to_replicate['Figure2']:
+        cfg = Cfg(N = 10000, do_random_v_space = False, zero_mean_exp = True, 
+                    load_results = False, save_fig = True, plot_type = 'numerical') 
+        exp_prob_wrapper(cfg) # Takes roughly 10 minutes to compute
+                              # To get some results faster, decrease N (and expect less smooth outputs)  
+    
+    if to_replicate['Figure3']:
+        cfg = Cfg(N = 10000, do_random_v_space = False, zero_mean_exp = False,  
+                    load_results = False, save_fig = True, plot_type = 'numerical') 
+        exp_prob_wrapper(cfg) # Takes roughly 10 minutes to compute
+
+    if to_replicate['Figure4']:
+        cfg = Cfg(N = 100000, do_random_v_space = True, zero_mean_exp = True, 
+                    load_results = False, save_fig = True, plot_type = 'numerical') 
+        exp_prob_wrapper(cfg) # Takes roughly 70 minutes to compute
+
+
+
+if __name__ == '__main__':
+    replicate()
